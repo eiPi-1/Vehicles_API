42b04b725b1dbe942a388c037f191c1a27ca9952
Mariyan M. Stoyanov <mstoyanov1@gmail.com>
2021-06-01 16:52:54 +0300

    Eureka server working

--- pricing-service/src/main/resources/data.sql
+++ pricing-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO price (currency, price, vehicleId) VALUES ('BGN', 85326.5, 1);
-INSERT INTO price (currency, price, vehicleId) VALUES ('EUD', 62251.6, 2);
-INSERT INTO price (currency, price, vehicleId) VALUES ('USD', 78324.24, 3);
+INSERT INTO pricedata (currency, price, vehicleId) VALUES ('BGN', 85326.5, 1);
+INSERT INTO pricedata (currency, price, vehicleId) VALUES ('EUD', 62251.6, 2);
+INSERT INTO pricedata (currency, price, vehicleId) VALUES ('USD', 78324.24, 3);
