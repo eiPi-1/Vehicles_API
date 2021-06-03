449e1a6a4df8ed4c4f0cf3f91dd67b663a141f36
Mariyan M. Stoyanov <mstoyanov1@gmail.com>
2021-06-03 22:49:30 +0300

    SQL working but Eureka client throw an error.

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -3,6 +3,7 @@ package com.udacity.pricing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
--- pricing-service/src/main/resources/data.sql
+++ pricing-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO price (currency, price, vehicle_id) VALUES ("BGN", 85326.5, 1);
-INSERT INTO price (currency, price, vehicle_id) VALUES ("EUD", 62251.6, 2);
-INSERT INTO price (currency, price, vehicle_id) VALUES ("USD", 78324.24, 3);
+INSERT INTO price (vehicle_id, currency, price) VALUES (1, 'BGN', 85326.5);
+INSERT INTO price (vehicle_id, currency, price) VALUES (2, 'EUR', 62251.6);
+INSERT INTO price (vehicle_id, currency, price) VALUES (3, 'USD', 78324.24);
